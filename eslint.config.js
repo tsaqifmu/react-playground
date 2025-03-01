d1c1d651b000c805e1af9df94d6a37c02f77f7f3
Ochaa <mushoddaqt@gmail.com>
2025-03-01 14:20:27 +0700

    fix(eslint): enable Prettier integration in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -28,7 +28,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'no-console': ['error'],
+      'no-console': ['error', { allow: ['warn', 'error'] }],
     },
   }
 );
